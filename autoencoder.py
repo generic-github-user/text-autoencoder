d796acafbb09c2c373a3b7c4010d24f319295897
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-07-07 11:00:06 -0400

    Create autoencoder.py

---
+++ autoencoder.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[156]:
+
+
+import tensorflow as tf
+from tensorflow.keras import utils
+from tensorflow.keras import layers
+from tensorflow.keras import losses
+from tensorflow.keras import preprocessing
+
+import numpy as np
+import matplotlib.pyplot as plt
+import string
+import pathlib
