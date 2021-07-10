e6e430c3b54d100f111b8ea3f76611ab48b65e36
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-07-09 23:18:37 -0400

    Testing zlib compression

--- comp_test.py
+++ comp_test.py
@@ -12,3 +12,27 @@ import string
 import random
 import base64
 import unicodedata
+
+
+# In[70]:
+
+
+a = zlib.compress(b'apples and oranges')
+# a = zlib.compress(b'a')
+# a[4] = 's'
+byteorder = 'big'
+# a = int.from_bytes(a, 'little') << 2
+num_bytes = len(a)
+a = int.from_bytes(a, byteorder)# + (2 ** 20)
+print(a)
+a = a.to_bytes(num_bytes, byteorder)
+# a = str(a).encode()
+print(zlib.decompress(a))
+
+b = zlib.compress(b'plums and pears')
+
+
+# In[142]:
+
+
+z = zlib.compressobj(level=9)
