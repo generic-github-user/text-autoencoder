92daae9cb6d5b3c26e61b7fed8d5f3463d8d120a
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-07-09 23:03:56 -0400

    add applicable text/string preprocessing functions

--- locality-sensitive-hashing.py
+++ locality-sensitive-hashing.py
@@ -24,3 +24,50 @@ nltk.download('words')
 
 
 word_list = words.words()
+
+
+# In[83]:
+
+
+seaborn.set_theme()
+
+
+# In[76]:
+
+
+ord('a')
+
+
+# In[56]:
+
+
+charset = ''.join([
+    string.ascii_lowercase,
+#     string.ascii_uppercase,
+#     string.digits,
+#     string.punctuation,
+    ' '
+])
+
+
+# In[57]:
+
+
+def pad(x, n=10):
+#     s = ' ' * n
+    return x + ' ' * (n - len(x))
+
+
+# In[58]:
+
+
+def one_hot(text, onehot=True):
+    text = text.lower()
+    encoded = []
+    for c in text:
+        if c in charset:
+            encoded.append(charset.index(c))
+        else:
+            encoded.append(charset.index(' '))
+#         encoded = np.expand_dims(encoded, 1)
+    return np.array(encoded)
