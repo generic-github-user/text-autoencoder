e96f4aa6a5f56366252e8fb2c288a54f4f3e71fa
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-07-09 23:02:00 -0400

    Create locality-sensitive-hashing.py

---
+++ locality-sensitive-hashing.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[98]:
+
+
+from scipy import spatial
+import string
+import numpy as np
+from fuzzywuzzy import fuzz, process
+import seaborn
+import matplotlib.pyplot as plt
+import random
