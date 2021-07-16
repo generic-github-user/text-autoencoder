edc906f4c4de8f5283b7b15c27d6c0d1e41369dc
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-07-15 22:24:03 -0400

    Add necessary imports for heatmap functions

--- heatmap.py
+++ heatmap.py
@@ -1,3 +1,7 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
 # Heatmap functions are from:
 # https://matplotlib.org/stable/gallery/images_contours_and_fields/image_annotated_heatmap.html
 
