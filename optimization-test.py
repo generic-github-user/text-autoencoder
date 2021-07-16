19a096f1f2305b63729413a10df1b73896eea061
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-07-16 02:30:25 -0400

    Add function for loading image to array of pixel values
    Adapted from https://stackoverflow.com/a/7769424/10940584

--- optimization-test.py
+++ optimization-test.py
@@ -8,3 +8,21 @@ from PIL import Image
 import numpy as np
 import nevergrad as ng
 import matplotlib.pyplot as plt
+
+
+# In[2]:
+
+
+# !dir
+
+
+# In[3]:
+
+
+# Via https://stackoverflow.com/a/7769424/10940584
+def load_image( infilename, scale=0.05) :
+#     img = Image.open( open(infilename, 'rb') )
+    img = Image.open(infilename)
+    img.load()
+    data = np.asarray( img.resize(tuple((np.array(img.size) * scale).round().astype(int))), dtype="int32" )
+    return data
